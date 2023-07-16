39d2959f28a4d855ab50fff2761d45267177054d
saulprl <phasecross@hotmail.com>
2023-07-15 20:17:26 -0700

    Add axios, add code exchange

--- server.js
+++ server.js
@@ -11,10 +11,31 @@ const REDIRECT_URL_WEB = process.env.GITHUB_WEB_REDIRECT;
 const REDIRECT_URL_MOBILE = process.env.GITHUB_MOBILE_REDIRECT;
 
 // Endpoint to handle GitHub web callback
-app.get("/oauth/callback/web", (req, res) => {
+app.get("/oauth/callback/web", async (req, res) => {
   const code = req.query.code;
-  const redirectURL = `${REDIRECT_URL_WEB}?code=${code}`;
-  res.redirect(redirectURL);
+  try {
+    const tokenResponse = await axios.post(
+      "https://github.com/login/oauth/access_token",
+      {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code: code,
+      },
+      {
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+
+    const accessToken = tokenResponse.data.access_token;
+    const redirectURL = `${REDIRECT_URL_WEB}?access_token=${accessToken}`;
+
+    res.redirect(redirectURL);
+  } catch (error) {
+    console.error(error);
+    res.redirect(REDIRECT_URL_WEB);
+  }
 });
 
 // Endpoint to handle GitHub mobile callback
